f95e2129d2cbb75ca2e5a962002c97796b52abf6
Shane Fast <shane.fast@athennian.com>
2023-04-07 22:23:00 -0600

    audit script

---
+++ 1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_2_Ensure_prelink_is_not_installed.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+output=$(dpkg-query -W -f='${binary:Package}\t${Status}\t${db:Status-Status}\n' prelink)
+
+if [[ "$output" == *"prelink unknown ok not-installed not-installed"* ] || [ "$output" == *"dpkg-query: no packages found matching prelink"* ]]; then
+  echo "pass"
+else
+  echo "FAIL"
+fi
