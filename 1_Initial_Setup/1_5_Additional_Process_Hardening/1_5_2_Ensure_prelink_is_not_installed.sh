ae5052f099a66c306aff4765187ad6395d416fbf
Shane Fast <shane.fast@athennian.com>
2023-04-08 01:05:50 -0600

    creating error file

--- 1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_2_Ensure_prelink_is_not_installed.sh
+++ 1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_2_Ensure_prelink_is_not_installed.sh
@@ -5,10 +5,10 @@ echo "----- 1.5.2 Ensure prelink is not installed audit. -----"
 echo ""
 
 prelink_error=$(dpkg-query -W -f='${binary:Package}\t${Status}\t${db:Status-Status}\n' prelink 2>&1)
-echo "$prelink_error" > reports/1_5_2.txt
+echo "$prelink_error" > reports/1_5_2.error.txt
 dpkg-query -W -f='${binary:Package}\t${Status}\t${db:Status-Status}\n' prelink > reports/1_5_2.txt
 cat reports/1_5_2.txt
-if [[ $(grep "not-installed" reports/1_5_2.txt) || $(grep "no packages found matching prelink" reports/1_5_2.txt) ]]; then
+if [[ $(grep "not-installed" reports/1_5_2.txt) || $(grep "no packages found matching prelink" reports/1_5_2.error.txt) ]]; then
   echo "pass"
   echo ""
 else
