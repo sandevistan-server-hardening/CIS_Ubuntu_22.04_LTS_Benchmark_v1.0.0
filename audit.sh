70f4b439bd7a6eb3942c33975bba1b488b3bc457
Shane Fast <shane.fast@athennian.com>
2023-04-07 22:48:32 -0600

    adding new scripts to top level test and audit scripts

--- audit.sh
+++ audit.sh
@@ -2,3 +2,6 @@
 
 # Hardness profile selection
 ./profile/hardnessProfileSelector.sh
+
+# 1 Initial Setup Audit
+./1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_Additional_Process_Hardening.sh
--- test.sh
+++ test.sh
@@ -2,3 +2,6 @@
 
 # Hardness profile selection test
 ./profile/hardnessProfileSelector.test.sh
+
+# 1 Initial Setup Tests
+./1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_Additional_Process_Hardening.test.sh
