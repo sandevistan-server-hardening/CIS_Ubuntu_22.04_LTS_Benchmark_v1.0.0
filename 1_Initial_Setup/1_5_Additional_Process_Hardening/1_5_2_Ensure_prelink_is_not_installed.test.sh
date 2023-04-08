258aa02a72a4382f7d743a179d33acb4ddd2c656
Shane Fast <shane.fast@athennian.com>
2023-04-07 22:23:06 -0600

    test script

---
+++ 1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_2_Ensure_prelink_is_not_installed.test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Out of the box test
+./1_5_2_Ensure_prelink_is_not_installed.sh > output.txt
+if [[ ! grep "prelink unknown ok not-installed not-installed" output.txt ] || [ ! grep "dpkg-query: no packages found matching prelink" output.txt ]; then
+  echo "Test passed: prelink not installed out of the box"
+else
+  echo "Test failed: prelink not installed out of the box"
+fi
+
+
+# Detecting if prelink is installed test
+apt install prelink -y
+
+./1_5_2_Ensure_prelink_is_not_installed.sh > output.txt
+if ! grep "prelink install ok installed    installed" output.txt; then
+  echo "Test passed: detecting if prelink is installed"
+else
+  echo "Test failed: detecting if prelink is installed"
+fi
+
+# after prelink purge test
+apt purge prelink
+./1_5_2_Ensure_prelink_is_not_installed.sh > output.txt
+if [[ ! grep "prelink unknown ok not-installed not-installed" output.txt ] || [ ! grep "dpkg-query: no packages found matching prelink" output.txt ]; then
+  echo "Test passed: prelink not installed out of the box"
+else
+  echo "Test failed: prelink not installed out of the box"
+fi
