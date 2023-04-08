d95de04b570b44e3ea52aee9adfbd7f3ab7f995a
Shane Fast <shane.fast@athennian.com>
2023-04-08 00:57:53 -0600

    caturing standard error in variable

--- 1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_2_Ensure_prelink_is_not_installed.sh
+++ 1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_2_Ensure_prelink_is_not_installed.sh
@@ -4,9 +4,10 @@ echo ""
 echo "----- 1.5.2 Ensure prelink is not installed audit. -----"
 echo ""
 
+prelink_error=$(dpkg-query -W -f='${binary:Package}\t${Status}\t${db:Status-Status}\n' prelink 2>&1)
 dpkg-query -W -f='${binary:Package}\t${Status}\t${db:Status-Status}\n' prelink > reports/1_5_2.txt
 cat reports/1_5_2.txt
-if [[ $(grep "not-installed" reports/1_5_2.txt) || $(grep "no packages found matching prelink" reports/1_5_2.txt) ]]; then
+if [[ $(grep "not-installed" reports/1_5_2.txt) || prelink_error ]]; then
   echo "pass"
   echo ""
 else
