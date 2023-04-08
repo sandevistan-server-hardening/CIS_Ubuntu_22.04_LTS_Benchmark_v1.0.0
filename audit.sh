ba1fce2349deeac36f0133ac70ff84f0783c1f03
Shane Fast <shane.fast@athennian.com>
2023-04-07 22:52:58 -0600

    pathing fixes

--- audit.sh
+++ audit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Hardness profile selection
-./profile/hardnessProfileSelector.sh
+./profile/hardnessProfileSelector.sh $1 $2
 
 # 1 Initial Setup Audit
-./1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_Additional_Process_Hardening.sh
+./1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_2_Ensure_prelink_is_not_installed.sh
--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Hardness profile selection test
-./profile/hardnessProfileSelector.test.sh
+./profile/hardnessProfileSelector.test.sh $1 $2
 
 # 1 Initial Setup Tests
-./1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_Additional_Process_Hardening.test.sh
+./1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_2_Ensure_prelink_is_not_installed.test.sh
