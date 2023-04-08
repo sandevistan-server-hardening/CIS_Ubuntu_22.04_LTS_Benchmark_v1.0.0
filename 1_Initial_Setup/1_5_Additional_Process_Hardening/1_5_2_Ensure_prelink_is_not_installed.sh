c2614d007a98913d3bdcce042d76328a4cd2b333
Shane Fast <shane.fast@athennian.com>
2023-04-07 23:17:07 -0600

    sending logs to reports directory

--- 1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_2_Ensure_prelink_is_not_installed.sh
+++ 1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_2_Ensure_prelink_is_not_installed.sh
@@ -4,9 +4,9 @@ echo ""
 echo "----- 1.5.2 Ensure prelink is not installed audit. -----"
 echo ""
 
-output=$(dpkg-query -W -f='${binary:Package}\t${Status}\t${db:Status-Status}\n' prelink)
-echo $output
-if [[ "$output" == *"prelink unknown ok not-installed not-installed"* || "$output" == *"dpkg-query: no packages found matching prelink"* ]]; then
+dpkg-query -W -f='${binary:Package}\t${Status}\t${db:Status-Status}\n' prelink > reports/1_5_2.txt
+cat reports/1_5_2.txt
+if [[ ! $(grep "prelink unknown ok not-installed not-installed" reports/1_5_2.txt) || ! $(grep "dpkg-query: no packages found matching prelink" reports/1_5_2.txt) ]]; then
   echo "pass"
 else
   echo "FAIL"
