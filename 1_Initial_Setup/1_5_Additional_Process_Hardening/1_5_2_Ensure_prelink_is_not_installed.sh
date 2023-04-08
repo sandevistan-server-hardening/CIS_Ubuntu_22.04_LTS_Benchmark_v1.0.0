ef00427a52f66f7fe827af6147ffc6c57693e72a
Shane Fast <shane.fast@athennian.com>
2023-04-08 00:23:23 -0600

    inverting logic

--- 1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_2_Ensure_prelink_is_not_installed.sh
+++ 1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_2_Ensure_prelink_is_not_installed.sh
@@ -6,7 +6,7 @@ echo ""
 
 dpkg-query -W -f='${binary:Package}\t${Status}\t${db:Status-Status}\n' prelink > reports/1_5_2.txt
 cat reports/1_5_2.txt
-if [[ ! $(grep "prelink unknown ok not-installed not-installed" reports/1_5_2.txt) || ! $(grep "dpkg-query: no packages found matching prelink" reports/1_5_2.txt) ]]; then
+if [[ $(grep "prelink unknown ok not-installed not-installed" reports/1_5_2.txt) || $(grep "dpkg-query: no packages found matching prelink" reports/1_5_2.txt) ]]; then
   echo "pass"
 else
   echo "FAIL"
