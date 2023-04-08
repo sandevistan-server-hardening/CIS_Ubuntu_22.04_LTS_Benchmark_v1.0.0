3445bf48fcdb966a07a283bbb997f85d4f63194f
Shane Fast <shane.fast@athennian.com>
2023-04-08 00:01:20 -0600

    ls test

--- 1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_2_Ensure_prelink_is_not_installed.test.sh
+++ 1_Initial_Setup/1_5_Additional_Process_Hardening/1_5_2_Ensure_prelink_is_not_installed.test.sh
@@ -22,7 +22,8 @@ echo ""
 
 # Get the directory of the currently executing script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
+ls
+ls $SCRIPT_DIR
 # Out of the box test
 .$SCRIPT_DIR/1_5_2_Ensure_prelink_is_not_installed.sh > output.txt
 if [[ $(grep "prelink unknown ok not-installed not-installed" output.txt) || $(grep "dpkg-query: no packages found matching prelink" output.txt) ]]; then
